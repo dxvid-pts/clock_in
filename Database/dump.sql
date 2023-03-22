b414377c72d96ecdcc48ee5001cd9621ca4853c0
philipp <philipp3923@icloud.com>
2023-03-22 15:19:28 +0100

    database schema

--- Database/dump.sql
+++ Database/dump.sql
@@ -1,5 +1,60 @@
-
 CREATE TABLE account
 (
-    id           INT PRIMARY KEY AUTO_INCREMENT
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    email      VARCHAR(255) NOT NULL,
+    password   VARCHAR(255),
+    role       VARCHAR(31)  NOT NULL,
+    last_login DATE,
+    blocked    BOOL
 );
+
+CREATE TABLE token
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    account_id INT          NOT NULL,
+    expiration DATE         NOT NULL,
+    content    VARCHAR(511) NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account (id)
+);
+
+CREATE TABLE manager_employee
+(
+    id          INT PRIMARY KEY AUTO_INCREMENT,
+    manager_id  INT NOT NULL,
+    employee_id INT NOT NULL,
+    FOREIGN KEY (manager_id) REFERENCES account (id),
+    FOREIGN KEY (employee_id) REFERENCES account (id)
+);
+
+CREATE TABLE work
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    account_id INT  NOT NULL,
+    begin      DATE NOT NULL,
+    end        DATE,
+    changed DATE NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account (id)
+);
+
+CREATE TABLE vacation
+(
+    id         INT PRIMARY KEY AUTO_INCREMENT,
+    account_id INT  NOT NULL,
+    begin      DATE NOT NULL,
+    end        DATE,
+    status VARCHAR(31),
+    changed DATE NOT NULL,
+    FOREIGN KEY (account_id) REFERENCES account (id)
+);
+
+CREATE TABLE profile
+(
+    account_id INT NOT NULL,
+    work_time INT NOT NULL,
+    begin_time DATE NOT NULL,
+    end_time DATE NOT NULL,
+    break_time INT NOT NULL,
+    vacation_days INT NOT NULL,
+    PRIMARY KEY (account_id),
+    FOREIGN KEY (account_id) REFERENCES account(id),
+)  WITH SYSTEM VERSIONING;
--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 # Clock In
 
-**Clock In** ist eine Gruppenarbeit im Rahmen der SWE-Vorlesung an der DHBW Stuttgart. Dies ist das Repository mit dem aktuellsten Stand des Codes.
+**Clock In** ist eine Gruppenarbeit im Rahmen der SWE-Vorlesung an der DHBW Stuttgart. Dies ist das Repository mit dem
+aktuellsten Stand des Codes.
 
 ## Geplante Funktionen
+
 Clock In ist eine Software zur Erfassung, Visualisierung und Verwaltung von Arbeitzeit.
-- **Nutzer** können ihre Arbeitszeit im Homeoffice oder in der Arbeit tracken und sehen in einem Dashboard wichtige Informationen bezglich ihres Arbeitsrhytmus. Zudem werden Überstunden automatisch getracked und es bestehen Funktionen zur Beantragung von Urlaub.
+
+- **Nutzer** können ihre Arbeitszeit im Homeoffice oder in der Arbeit tracken und sehen in einem Dashboard wichtige
+  Informationen bezglich ihres Arbeitsrhytmus. Zudem werden Überstunden automatisch getracked und es bestehen Funktionen
+  zur Beantragung von Urlaub.
 - **Manager** können mehrere Nutzer verwalten, Anträge bearbeitet und Übersichten der Arbeitszeiten erstellen.
 - **Administratoren** können neue Nutzer anmelden und Zugriffsrechte von Managern verwalten.
 
 ## Umsetzung
-Die Umsetzung des Projektes wird mithilfe von Jira getracked. [Link zum Agile Board](https://swezwei.atlassian.net/jira/software/projects/SZ/boards/1)
+
+Die Umsetzung des Projektes wird mithilfe von Jira
+getracked. [Link zum Agile Board](https://swezwei.atlassian.net/jira/software/projects/SZ/boards/1)
 
