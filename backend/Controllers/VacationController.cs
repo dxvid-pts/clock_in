4e5eb319b917117c7284e03e8f898e8e8467e7ed
philipp <philipp3923@icloud.com>
2023-03-23 11:01:08 +0100

    VacationController.cs updated

--- backend/Controllers/VacationController.cs
+++ backend/Controllers/VacationController.cs
@@ -17,9 +17,13 @@ public class VacationController : ControllerBase
     /// Creates a new Vacation Request
     /// </summary>
     /// <param name="begin">Timestamp which defines the begin of the vacation</param>
-    /// <param name="end">Timestamp which defines the begin of the vacation</param>
+    /// <param name="end">Timestamp which defines the end of the vacation</param>
     /// <returns></returns>
+    /// <response code="201">Submission successful</response>
+    /// <response code="400">Submission rejected</response>
     [HttpPost(Name = "PostVacation")]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public IActionResult Post(int begin, int end)
     {
         return Ok();
