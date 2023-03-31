751172b89165006ce9926bb120250eca4a92b454
philipp <philipp3923@icloud.com>
2023-03-31 15:31:03 +0200

    init_db.py comments

--- backend/init_db.py
+++ backend/init_db.py
@@ -2,23 +2,25 @@ import sys
 
 def rename_file(host, port, db, user, psswrt):
     with open('appsettings.json', 'r+') as f:
-        text = f.read()
-        f.seek(0)
-        print(text)
-        try:
+        text = f.read() # read the text
+        f.seek(0) # reset pointer
+        
+
+    try: # to catch regex errors
             (before_text,focus_text) = text.split("\"Database\": \"")
             (focus_text,after_text) = focus_text.split("\"")
         except:
             print("String decoding failed")
             return 1
             
-        focus_text = f"Server={host}; Database={db}; Password={psswrt}; User={user}; Port={user}"
-        
-        file_text = before_text + "\"Database\": \"" + focus_text + "\"" + after_text
-        print(file_text)
-        f.truncate(0)
-        f.write(file_text)
-        return 0
+        focus_text = f"Server={host}; Database={db}; Password={psswrt}; User={user}; Port={user}" # insert values
+        file_text = before_text + "\"Database\": \"" + focus_text + "\"" + after_text # restore full text
+
+        f.truncate(0) # clear file
+        f.write(file_text) # rewrite file
+    
+    print("appsettings.json has been modified successfully")
+    return 0
        
          
  
