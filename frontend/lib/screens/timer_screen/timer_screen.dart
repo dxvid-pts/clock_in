c23601ff258f01c668abda8c694126c04db63735
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-04-09 16:40:19 +0200

    base layout

--- frontend/lib/screens/timer_screen/timer_screen.dart
+++ frontend/lib/screens/timer_screen/timer_screen.dart
@@ -6,12 +6,52 @@ class TimerScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Timer"),
-      ),
-      body: const Center(
-        child: Text("Timer"),
-      ),
-    );
+        body: Column(
+      children: [
+        Expanded(
+          child: Container(
+            color: Theme.of(context).primaryColor,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              children: [
+                Text(
+                  'Timer',
+                  style: Theme.of(context).textTheme.headline1,
+                ),
+                SizedBox(height: 20),
+                //hour:minute:second timer
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      '00',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                    Text(
+                      ':',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                    Text(
+                      '00',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                    Text(
+                      ':',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                    Text(
+                      '00',
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        Expanded(child: Container()),
+      ],
+    ));
   }
 }
