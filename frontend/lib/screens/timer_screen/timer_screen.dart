d5453917805b0581bd377af4c9f9a01ba0149a99
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-04-09 16:29:34 +0200

    add timer screen

---
+++ frontend/lib/screens/timer_screen/timer_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class TimerScreen extends StatelessWidget {
+  const TimerScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Timer"),
+      ),
+      body: const Center(
+        child: Text("Timer"),
+      ),
+    );
+  }
+}
