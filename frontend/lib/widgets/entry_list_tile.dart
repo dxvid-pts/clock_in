059b45e6bcbf73d9981d17e753660f056b4f07af
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-04-09 22:58:06 +0200

    add color dot

--- frontend/lib/widgets/entry_list_tile.dart
+++ frontend/lib/widgets/entry_list_tile.dart
@@ -6,16 +6,18 @@ class EntryListTile extends StatelessWidget {
     required this.title,
     required this.subtitle,
     required this.duration,
+    this.color,
   }) : super(key: key);
 
   final String title;
   final String subtitle;
   final double duration;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.symmetric( vertical: 5),
+      margin: const EdgeInsets.symmetric(vertical: 5),
       //rounded corners
       decoration: const BoxDecoration(
         color: Color(0xFFf9f9f9),
@@ -24,8 +26,28 @@ class EntryListTile extends StatelessWidget {
         ),
       ),
       child: ListTile(
-        title: Text(title),
-        subtitle: Text(subtitle),
+        leading: color == null
+            ? null
+            : Container(
+                width: 2,
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "•",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: color!,
+                  ),
+                ),
+              ),
+        minLeadingWidth: 0,
+        title: Text(
+          title,
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        subtitle: Text(
+          subtitle,
+          style: Theme.of(context).textTheme.bodySmall,
+        ),
         trailing: Text("${duration.toStringAsFixed(2)}h"),
       ),
     );
