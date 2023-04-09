2057326394070ca7cd5b73006129a2fcbfacfed7
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-04-09 19:48:15 +0200

    add entry list tile

---
+++ frontend/lib/widgets/entry_list_tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class EntryListTile extends StatelessWidget {
+  const EntryListTile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
