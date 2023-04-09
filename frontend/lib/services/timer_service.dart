6d3d7c7fad963b74097005e966c1499dbb49d10c
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-04-09 16:35:53 +0200

    add timer service

---
+++ frontend/lib/services/timer_service.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final timerProvider =
+    ChangeNotifierProvider<TimerNotifier>((ref) => TimerNotifier());
+
+class TimerNotifier extends ChangeNotifier {
+  double hours = 0;
+  double minutes = 0;
+  double seconds = 0;
+
+  Timer? _t;
+
+  TimerNotifier() {}
+
+  void startTimer() {
+    if (_t != null) {
+      return;
+    }
+
+    //periodically update the timer every second
+    _t = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (seconds < 59) {
+        seconds++;
+      } else {
+        seconds = 0;
+        if (minutes < 59) {
+          minutes++;
+        } else {
+          minutes = 0;
+          hours++;
+        }
+      }
+      notifyListeners();
+    });
+  }
+
+  void stopTimer() {
+    _t?.cancel();
+    _t = null;
+  }
+}
