55c70b63551a6cee7ed09fbf1c5b20283494aae1
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-04-19 14:00:48 +0200

    add start / end field

--- frontend/lib/screens/overview_screen/widgets/planning_dialog.dart
+++ frontend/lib/screens/overview_screen/widgets/planning_dialog.dart
@@ -7,13 +7,81 @@ void showPlanningDialog(BuildContext context) {
   );
 }
 
-class PlanningDialog extends StatelessWidget {
+class PlanningDialog extends StatefulWidget {
   const PlanningDialog({super.key});
 
   @override
+  State<PlanningDialog> createState() => _PlanningDialogState();
+}
+
+class _PlanningDialogState extends State<PlanningDialog> {
+  DateTime? _startDate;
+  DateTime? _endDate;
+
+  @override
   Widget build(BuildContext context) {
-    return const Dialog(
-      child: Placeholder(),
+    return AlertDialog(
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          //Title: "Planning"
+          Text("Planning", style: Theme.of(context).textTheme.titleMedium),
+
+          const SizedBox(height: 10),
+
+          //button in the style of a textfield
+          TextField(
+            controller: TextEditingController(
+              text: _startDate?.toIso8601String() ?? "",
+            ),
+            onTap: () {
+              //show date picker
+              showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime.now().subtract(const Duration(days: 365)),
+                lastDate: DateTime.now().add(const Duration(days: 365)),
+              ).then((value) {
+                setState(() {
+                  if (value != null) _startDate = value;
+                });
+              });
+            },
+            readOnly: true,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "Start",
+            ),
+          ),
+
+          const SizedBox(height: 10),
+
+          //end date
+          TextField(
+            controller: TextEditingController(
+              text: _startDate?.toIso8601String() ?? "",
+            ),
+            onTap: () {
+              //show date picker
+              showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime.now().subtract(const Duration(days: 365)),
+                lastDate: DateTime.now().add(const Duration(days: 365)),
+              ).then((value) {
+                setState(() {
+                  if (value != null) _endDate = value;
+                });
+              });
+            },
+            readOnly: true,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: "End",
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
