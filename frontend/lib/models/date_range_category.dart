38b01ada2acbebdc37e11f148e83d1ddbb64efa0
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-05-03 15:37:25 +0200

    add enum adapter

--- frontend/lib/models/date_range_category.dart
+++ frontend/lib/models/date_range_category.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/services.dart';
+import 'package:hive/hive.dart';
 
+@HiveType(typeId: 14, adapterName: 'DateRangeCategoryAdapter')
 enum DateRangeCategory {
+  @HiveField(0)
   vacation,
+  @HiveField(1)
   sick,
+  @HiveField(2)
   remote,
+  @HiveField(3)
   office,
 }
 
