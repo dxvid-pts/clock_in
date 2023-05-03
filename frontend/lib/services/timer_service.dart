8d1b87cd3963803b954da8ac0af5687d369f01e5
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-05-03 16:00:04 +0200

    count down and if reached count up

--- frontend/lib/services/timer_service.dart
+++ frontend/lib/services/timer_service.dart
@@ -9,6 +9,8 @@ final timerProvider =
 });
 
 class TimerNotifier extends ChangeNotifier {
+  final int hoursPerDay;
+
   int hours = 0;
   int minutes = 0;
   int seconds = 0;
@@ -17,10 +19,11 @@ class TimerNotifier extends ChangeNotifier {
 
   bool get wasRunningOnce => _t != null;
   bool get isRunning => _t?.isActive ?? false;
+  bool hasReachedZero = false;
 
   DateTime? startTime;
 
-  TimerNotifier(int hoursPerDay) {
+  TimerNotifier(this.hoursPerDay) {
     hours = hoursPerDay;
   }
 
@@ -35,15 +38,38 @@ class TimerNotifier extends ChangeNotifier {
 
     //periodically update the timer every second
     _t = Timer.periodic(const Duration(seconds: 1), (timer) {
-      if (seconds < 59) {
-        seconds++;
-      } else {
-        seconds = 0;
-        if (minutes < 59) {
-          minutes++;
+      //count down from the start time to 00:00:00; if the timer has reached 00:00:00, then count up
+
+      if (!hasReachedZero) {
+        if (seconds > 0) {
+          seconds--;
         } else {
-          minutes = 0;
-          hours++;
+          if (minutes > 0) {
+            minutes--;
+            seconds = 59;
+          } else {
+            if (hours > 0) {
+              hours--;
+              minutes = 59;
+              seconds = 59;
+            } else {
+              //timer has reached 00:00:00, now reverse and count up
+              hasReachedZero = true;
+              notifyListeners();
+            }
+          }
+        }
+      } else {
+        if (seconds < 59) {
+          seconds++;
+        } else {
+          seconds = 0;
+          if (minutes < 59) {
+            minutes++;
+          } else {
+            minutes = 0;
+            hours++;
+          }
         }
       }
       notifyListeners();
@@ -62,10 +88,11 @@ class TimerNotifier extends ChangeNotifier {
   void resetTimer() {
     _t?.cancel();
     _t = null;
-    hours = 0;
+    hours = hoursPerDay;
     minutes = 0;
     seconds = 0;
     startTime = null;
+    hasReachedZero = false;
     notifyListeners();
   }
 }
