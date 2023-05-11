daa3a2406e53c34dfee48f849a169a81b3dfa724
David Peters <46222898+dxvid-pts@users.noreply.github.com>
2023-05-11 09:14:09 +0200

    remove comment

--- frontend/lib/services/employee_service.dart
+++ frontend/lib/services/employee_service.dart
@@ -2,7 +2,6 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/models/employee.dart';
 import 'package:frontend/models/user.dart';
-//import firstwhereor null extension
 
 final employeeProvider = ChangeNotifierProvider.family<EmployeeNotifier, User>(
     (ref, user) => EmployeeNotifier(user));
